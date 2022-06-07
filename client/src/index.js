908a22e546cf5d6dc398825b454992043bdfbb92
lazycoder1 <gautamgsabhahit@gmail.com>
2022-06-07 15:28:49 +0530

    adding chat starter code

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Chat } from "./components/chat/Chat";
+import Transaction from "./components/transaction/transaction";
+import { Container, Row, Col } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+            <Row style={{display:"flex"}}>
+                <Col style={{display:"contents"}}>
+                    <Chat />
+                </Col>
+
+                <Col> <Transaction/></Col>
+            </Row>
+        
+    </React.StrictMode>,
+    // document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
