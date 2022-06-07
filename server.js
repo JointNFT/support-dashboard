cee11680371ffbf17fcf35978bd41d48221c3888
lazycoder1 <gautamgsabhahit@gmail.com>
2022-06-07 17:21:51 +0530

    added socket server and address cors

--- server.js
+++ server.js
@@ -1,13 +1,91 @@
 const express = require("express");
 const path = require('path');
-const app = express();
+const cors = require('cors');
+
+var app = express()
+
+// Middleware
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(cors())
+
 const port = process.env.PORT || 3000;
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+var server = require('http').createServer(app)
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+var io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:3000"
+      }
+});
+var io = io.listen(server);
+
+var STATIC_CHANNELS = [{
+    name: 'Global chat',
+    participants: 0,
+    id: 1,
+    sockets: []
+}, {
+    name: 'Funny',
+    participants: 0,
+    id: 2,
+    sockets: []
+}];
+
+
+
+io.on('connection', (socket) => { // socket object may be used to send specific messages to the new connected client
+    console.log('new client connected');
+    socket.emit('connection', null);
+    socket.on('channel-join', id => {
+        console.log('channel join', id);
+        STATIC_CHANNELS.forEach(c => {
+            if (c.id === id) {
+                if (c.sockets.indexOf(socket.id) == (-1)) {
+                    c.sockets.push(socket.id);
+                    c.participants++;
+                    io.emit('channel', c);
+                }
+            } else {
+                let index = c.sockets.indexOf(socket.id);
+                if (index != (-1)) {
+                    c.sockets.splice(index, 1);
+                    c.participants--;
+                    io.emit('channel', c);
+                }
+            }
+        });
+
+        return id;
+    });
+
+    socket.on('create-account', message => {
+
+    });
+    
+    socket.on('send-message', message => {
+        io.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        STATIC_CHANNELS.forEach(c => {
+            let index = c.sockets.indexOf(socket.id);
+            if (index != (-1)) {
+                c.sockets.splice(index, 1);
+                c.participants--;
+                io.emit('channel', c);
+            }
+        });
+    });
+
+});
+
+/**
+ * @description This methos retirves the static channels
+ */
+app.get('/getChannels', (req, res) => {
+    res.json({
+        channels: STATIC_CHANNELS
+    })
 });
 
 app.get("/api", (req, res) => {
@@ -19,6 +97,10 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
 });
