be92e5e0eb4b6c7aaafcf9ea4b8865cb15a15883
lazycoder1 <gautamgsabhahit@gmail.com>
2022-06-08 15:38:08 +0530

    adding transaction fetching

--- client/src/components/transaction/addressForm.js
+++ client/src/components/transaction/addressForm.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 const AddressForm = (props) => {
-    let state = {'userAddress': "0x5c146cd18fa53914580573c9b9604588529406ca", contractAddresses:"0xad337077480134028b7c68af290e891ce28076eb"}
-    console.log(props);
-    const handleInput = (e) => {
-        state[e.target.id] = e.target.value;
+    let [userAddress, setUserAddress] = useState('0x5c146cd18fa53914580573c9b9604588529406ca');
+    let [contractAddresses, setContractAddresses] = useState('0xad337077480134028b7c68af290e891ce28076eb');
+
+    const handleUserAddresInput = (e) => {
+        setUserAddress(e.target.value);
     } 
+
+    const handleContractAddressInput = (e) => {
+        setContractAddresses(e.target.value);
+    } 
+
     const send = (e) => {
-        console.log(e);
+        props.fetchTx(userAddress, contractAddresses);
     }
 
     return (
         <div className="address-input">
-                        <input type="text" id="userAdderss" onChange={handleInput} value={state.userAddress} ></input>
-                        <input type="text" id="contractAddress" onChange={handleInput} value={state.contractAddresses} ></input>
+                        <input type="text" id="userAdderss" onChange={handleUserAddresInput} value={userAddress} ></input>
+                        <input type="text" id="contractAddress" onChange={handleContractAddressInput} value={contractAddresses} ></input>
                         <button onClick={send}>Fetch Transactions</button>
         </div>
     );
--- client/src/components/transaction/transaction.js
+++ client/src/components/transaction/transaction.js
@@ -1,33 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import './transaction.css'
-import Accordion from 'react-bootstrap/Accordion'
 import AddressForm from "./addressForm";
+import TransactionList from "./transactionList";
 
 const Transaction = () => {
-    let state = {'userAddress': "0x5c146cd18fa53914580573c9b9604588529406ca", contractAddresses:"0xad337077480134028b7c68af290e891ce28076eb"}
+    let [transactions, setTransactions] = useState([]);
+
+    const fetchTransactions = async (userAddress, contractAddresses) => {
+        fetch('/transactions?userAddress='+userAddress+'&contractAddresses='+contractAddresses).then(async response => {
+            let data = await response.json();
+            setTransactions(data.filteredTransactions);
+            console.log(transactions);
+        })
+    }
+
     return (
         <div>
-        <AddressForm random='props'/>
-        <Accordion defaultActiveKey="0" >
-            <Accordion.Item eventKey="0">
-                <Accordion.Header >Accordion Item #1</Accordion.Header>
-                <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-                <Accordion.Header>Accordion Item #2</Accordion.Header>
-                <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-            </Accordion.Item>
-        </Accordion>
+        <AddressForm fetchTx={fetchTransactions}/>
+        <TransactionList transactions={transactions}/>
+        
         </div>
     );
 };
