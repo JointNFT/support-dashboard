601c70e0acf0d1153a6d4eb03fa1e31107c82e21
lazycoder1 <gautamgsabhahit@gmail.com>
2022-06-09 14:06:42 +0530

    adding create account page

--- client/src/components/chat/CreateAccountForm.js
+++ client/src/components/chat/CreateAccountForm.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./createAccountForm.css";
+import { Button } from "react-chat-elements";
 
 export class CreateAccountForm extends React.Component {
     state = { userAddress: "", accessToken: "" };
@@ -18,10 +20,12 @@ export class CreateAccountForm extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="entry-page">
                 <div>
+                    <label>Please enter your wallet address to start talking !</label>
                     <input type="text" id="userAdderss" onChange={this.handleUserAddressInput} value={this.state.userAddress}></input>
-                    <button onClick={this.createAccount}>Create Account</button>
+                    <br/>
+                    <Button onClick={this.createAccount} text="Start Chatting"></Button>
                 </div>
             </div>
         );
