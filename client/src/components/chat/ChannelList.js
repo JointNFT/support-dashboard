bc58ba5b3ca884463da3da7cf589a33b0736952c
lazycoder1 <gautamgsabhahit@gmail.com>
2022-06-10 21:51:01 +0530

    fix conflict

--- client/src/components/chat/ChannelList.js
+++ client/src/components/chat/ChannelList.js
@@ -20,12 +20,8 @@ export class ChannelList extends React.Component {
                         avatar: "https://storage.googleapis.com/opensea-static/opensea-profile/"+((parseInt(c.userAddress)%30)+1)+".png",
                         alt: "Some DP",
                         title: c.userAddress,
-                        date: new Date(c?.messages != null ? c.messages[c.messages.length - 1].timestamp : c?.lastMessage?.timestamp),
                         subtitle: c?.messages != null ? c.messages[c.messages.length - 1].message : c?.lastMessage?.message,
-<<<<<<< HEAD
-=======
                         date: new Date(c?.messages != null ? c.messages[c.messages.length - 1].timestamp : c?.lastMessage?.timestamp),
->>>>>>> 8756e1e (minor UI changes)
                         unread: c.unread != null ? c.unread : 0
                     };
             });
