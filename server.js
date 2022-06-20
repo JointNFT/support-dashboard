704686df86964c740c22fe4faa0f7c03be527ce2
pitLord1125 <mickeyglass.1125@gmail.com>
2022-06-20 16:19:27 +0900

    error fix

--- server.js
+++ server.js
@@ -3,54 +3,43 @@ const path = require('path');
 const cors = require('cors');
 require("dotenv").config({path: './.env'});
 const chatHandlers = require("./utils/chatHandlers");
-const { Client, Intents } = require("discord.js");
 const Web3 = require("web3");
 const utils = require("./utils/transactionDecoders");
 const axios = require("axios");
-const { start } = require("repl");
-const discord_token = process.env.DISCORD_TOKEN;
+
 
 var app = express()
 
 const w3 = new Web3(new Web3.providers.HttpProvider('https://rpcapi.fantom.network'))
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-client.login(discord_token);
-client.on("ready", () => {
-    console.log("client is ready");
-});
-
 // Middleware
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(cors())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 var server = require('http').createServer(app)
 
 var io = require('socket.io')(server, {
     cors: {
-        origin: "*"
+        origin: "localhost:3002"
       }
 });
 var io = io.listen(server);
 
 
+
 io.on('connection', (socket) => { // socket object may be used to send specific messages to the new connected client
     console.log('new client connected');
     socket.emit('connection', null);
-
-    socket.on('test',(arg)=>{
-        socket.emit('response','online');
-    })
     
     socket.on('create-account', data => {
-        console.log('create account', data);
-        if (data == null || data.userAddress == null || data.accessToken == null) {
+        console.log('data', data);
+        if (data == null || data.address == null || data.accessToken == null) {
             return;
         }
-        chatHandlers.createNewUser(data.userAddress, data.accessToken);
-        io.emit('new-account', {userAddress: data.userAddress, accessToken: data.accessToken});
+        chatHandlers.createNewUser(data.address, data.accessToken);
+        io.emit('new-account', {userAddress: data.address, accessToken: data.accessToken});
     });
     
     socket.on('send-message', data => {
@@ -59,7 +48,6 @@ io.on('connection', (socket) => { // socket object may be used to send specific 
             io.emit('message', 'errored out');
         }
         chatHandlers.handleCustomerMessage(data.address, data.message, data.accessToken, data.to, data.from);
-        chatHandlers.pushToDiscord(data, client);
         io.emit('message', data);
     });
 
@@ -85,9 +73,9 @@ app.get('/getUsers', async (req, res) => {
 })
 
 app.get('/getMessages', async (req, res) => {
-    
+    console.log(req.query);
     chatMessages = await chatHandlers.getMessages(req.query.address, req.query.accessToken);
-    
+    console.log(chatMessages);
     res.send(JSON.stringify({'messages': chatMessages}))
 })
 // Have Node serve the files for our built React app
@@ -103,38 +91,38 @@ app.get("/api", (req, res) => {
 });
 
 app.get('/transactions', async function (req, res) {
-    
+    console.log(req.query);
     let contractAddresses = req?.query?.contractAddresses.split(',');
-   
+    console.log(contractAddresses);
     let address = req?.query?.userAddress;
-    let chain = req?.query?.chain
     if (address == null) {
         res.send({'error':'Please enter the address'});
         return;
     }
-    let startBlock = "0"
+    let apiKey = process.env.API_KEY;
+    let startBlock = "34911344"
 
-    const userTransactions = await utils.getTransactions(address, startBlock, chain);
-    
+    let url="https://api.ftmscan.com/api?module=account&action=txlist&address="+address+"&startblock="+startBlock+"&endblock=99999999&sort=asc&apikey="+apiKey
+
+    const axiosRes = await axios.get(url);
+    const userTransactions = axiosRes?.data?.result;
+
     if (userTransactions == null) {
         res.send({'error':'couldnt fetch transactions for user'});
     }
     
-    let filteredTransactions = utils.filter_for_useful_transactions(userTransactions, contractAddresses)
-    
-    let populatedTransactions = await utils.populateTransactions(filteredTransactions, chain);
-    res.send(JSON.stringify({filteredTransactions: populatedTransactions}))
-})
-
-app.get('/test', (req, res) => {
-    chatHandlers.pushToDiscord({accessToken: "some-token", message:"need help", "to":"support", from:"0xe95", userAddress:"0xe95"}, client);
-    res.send('hehe');
+    console.log(contractAddresses);
+    filteredTransactions = utils.filter_for_useful_transactions(userTransactions, contractAddresses)
+    res.send(JSON.stringify({filteredTransactions}))
 })
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
+
+
+
 
 server.listen(port, () => {
     console.log(`listening on *:${port}`);
--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,7 @@
 const AWS = require("aws-sdk");
 
+console.log(process.env.AWS_ACCESS_KEY);
+console.log(process.env.AWS_SECRET_ACCESS_KEY);
 
 // Update AWS config
 AWS.config.update({
@@ -11,95 +13,4 @@ AWS.config.update({
 // Create DynamoDB service object
 const db = new AWS.DynamoDB.DocumentClient({ apiVersion: "latest" });
 
-const updateUser = async (userAddress, accessToken, lastMessage = {}) => {
-  let dbParams = {
-      TableName: "ChatUsers",
-      Item: {
-          userAddress: userAddress,
-          accessToken: accessToken,
-          lastMessage: lastMessage,
-      },
-  };
-
-  let response = await db.put(dbParams).promise();
-  console.log("response", await response);
-  return await response;
-};
-
-const getUser = async (userAddress, accessToken) => {
-  const params = {
-      TableName: "ChatUsers",
-      KeyConditionExpression: "userAddress = :pkey and accessToken = :skey",
-      ExpressionAttributeValues: {
-          ":pkey": userAddress,
-          ":skey": accessToken,
-      },
-  };
-  const res = await db.query(params).promise();
-  const resPayload = await res;
-  const user = resPayload.Items != [] ? resPayload.Items[0] : null;
-  return user;
-};
-
-const getUsers = async (accessToken) => {
-  const params = {
-      TableName: "ChatUsers",
-      IndexName: "accessToken-userAddress-index",
-      KeyConditionExpression: "accessToken = :skey",
-      ExpressionAttributeValues: {
-          ":skey": accessToken,
-      },
-  };
-  const res = await db.query(params).promise();
-  const resPayload = await res;
-  const user = resPayload.Items != [] ? resPayload.Items : null;
-  return user;
-};
-
-const getDiscordSettings = async (accessToken) => {
-  const params = {
-      TableName: "DiscordIntegration",
-      KeyConditionExpression: "accessToken = :pkey",
-      ExpressionAttributeValues: {
-          ":pkey": accessToken,
-      },
-  };
-  const res = await db.query(params).promise();
-  const resPayload = await res;
-  const discordServerId = resPayload.Items != [] ? resPayload.Items[0] : null;
-  return discordServerId;
-};
-
-const storeMessages = async (userAddress, accessToken, message, to, from) => {
-  const params = {
-      TableName: "SupportMessages",
-      Item: {
-          userAddress: userAddress,
-          timestamp: +new Date(),
-          accessToken: accessToken,
-          from: from,
-          to: to,
-          message: message,
-      },
-  };
-  let response = await db.put(params).promise();
-  response = await updateUser(userAddress, accessToken, params.Item);
-  return await response;
-};
-
-const getMessages = async (userAddress, accesToken) => {
-  const params = {
-      TableName: "SupportMessages",
-      KeyConditionExpression: "userAddress = :pkey",
-      FilterExpression: "accessToken= :aToken",
-      ExpressionAttributeValues: {
-          ":pkey": userAddress,
-          ":aToken": accesToken,
-      },
-  };
-  const res = await db.query(params).promise();
-  const messages = (await res)?.Items;
-  return messages;
-};
-
-module.exports = {getMessages, storeMessages, getUser, getUsers, updateUser, getDiscordSettings};
+module.exports = {db};
