23367ed8312a7ffeaee525a1503576be61c1de97
linhtran <tranchilinh.0312@gmail.com>
2022-08-02 15:44:58 +0700

    create server URL that can be re-used in other components

---
+++ client/src/config/index.js
@@ -0,0 +1,1 @@
+export { default as SERVER } from './server'
---
+++ client/src/config/server.js
@@ -0,0 +1,5 @@
+// Dev environment
+const SERVER = process.env.REACT_APP_SERVER_URL_TEST;
+// Prod environment
+//const SERVER = process.env.REACT_APP_SERVER_URL;
+export default SERVER
