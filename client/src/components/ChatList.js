0eb43d279be2faf7463ca5c4958848b6438987cc
linhtran <tranchilinh.0312@gmail.com>
2022-08-04 22:09:57 +0700

    add conversation filter functionality

--- client/src/components/ChatList.js
+++ client/src/components/ChatList.js
@@ -1,28 +1,56 @@
 import {
   Box,
   Heading,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
 } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import ChatBox from "./ChatBox";
 import { format } from "timeago.js";
 import { useState } from "react";
 import { getChannelList } from "../contexts/ChannelContext";
+import { TransactionStatusTab as ConversationStatusTab } from "./Transaction";
 
+const CONVERSATION_FILTER = {
+  NEWEST: "Newest",
+  OLDEST: "Oldest",
+};
+const getTime = (date) =>{ 
+  const d = new Date(date);
+  if(isNaN(d)) return '';
+  return Math.floor(d.getTime() / 1000)
+};
+const sortConversations = (conversations, filter) => {
+  let result = conversations ? [...conversations] : [];
+  switch (filter) {
+    case CONVERSATION_FILTER.NEWEST:
+      result = result.sort((a, b) => getTime(b.date) - getTime(a.date));
+      break;
+    case CONVERSATION_FILTER.OLDEST:
+      result = result.sort((a, b) => getTime(a.date) - getTime(b.date));
+      break;
+    default:
+      break;
+  }
+  return result;
+};
 const ChatList = (props) => {
   const [isActive, setIsActive] = useState(null);
   const [list, setList] = useState([]);
+  const [currentList, setCurrentList] = useState([]);
+
+  const [filter, setFilter] = useState(CONVERSATION_FILTER.NEWEST);
+  const handleFilterConversations = React.useCallback((value) => {
+    setFilter(value);
+  }, []);
 
   useEffect(() => {
-    console.log(props)
     const list = getChannelList(props);
-    console.log(list);
     setList(list);
+    console.log(list)
   }, [props.channels]);
+
+  useEffect(() => {
+    setCurrentList(sortConversations(list, filter));
+  }, [filter, list]);
 
   const handleClick = (channelId, id) => {
     props.onSelectChannel(channelId);
@@ -44,91 +72,53 @@ const ChatList = (props) => {
         Conversations / {props.heading}
       </Heading>
       <Heading as="h4" size={"md"} my="3" color="#2C5282">
-        {props.heading}
+        {props.heading}{' '}{list?.length && `(${list.length})` }
       </Heading>
-      <Tabs variant="unstyled" colorScheme="blue">
-        <TabList mx={"auto"} justifyContent="center" gap="10px">
-          {/* <Tab
-            style={{ color: "#2C5282" }}
-            bg={"#bee3f8"}
-            width="60px"
-            fontSize={"12px"}
-          >
-            Newest
-          </Tab>
-          <Tab
-            style={{ color: "#2C5282" }}
-            bg={"#bee3f8"}
-            width="60px"
-            fontSize={"12px"}
-          >
-            Oldest
-          </Tab>
-          <Tab
-            style={{ color: "#2C5282" }}
-            bg={"#bee3f8"}
-            width="60px"
-            fontSize={"12px"}
-          >
-            Longest
-          </Tab>
-          <Tab
-            style={{ color: "#2C5282" }}
-            bg={"#bee3f8"}
-            width="60px"
-            fontSize={"12px"}
-          >
-            Shortest
-          </Tab> */}
-        </TabList>
+        {Object.values(CONVERSATION_FILTER).map((k) => (
+          <ConversationStatusTab
+            key={k}
+            title={k}
+            active={filter === k}
+            onClick={() => handleFilterConversations(k)}
+          />
+        ))}
         <Box
           h={"70vh"}
           marginTop="20px"
           sx={{
             "&::-webkit-scrollbar": {
-              width: "10px",
+              width: "6px",
               borderRadius: "8px",
               backgroundColor: `rgba(0, 0, 0, 0.05)`,
             },
             "&::-webkit-scrollbar-thumb": {
-              backgroundColor: `rgba(255, 255, 255, 1)`,
+              backgroundColor: `#90CDF4`,
+              borderRadius: "2px",
             },
           }}
           overflowY={"scroll"}
+          paddingRight="10px"
         >
-          <TabPanels>
-            <TabPanel>
-              {list?.length ? (
-                list.map((conversationInfo) => (
-                  <ChatBox
-                    isActive={isActive}
-                    setIsActive={setIsActive}
-                    src={conversationInfo.avatar}
-                    name={conversationInfo.title.slice(0, 10) + "..."}
-                    info={conversationInfo.subtitle}
-                    unreadCnt={conversationInfo.unread}
-                    lastActivityTime={format(conversationInfo.date)}
-                    handleClick={() => handleClick(conversationInfo.title)}
-                    id={conversationInfo.title}
-                  />
-                ))
-              ) : (
-                <Heading
-                  as="h5"
-                  fontSize={"22px"}
-                  color="#2C5282"
-                  align={"center"}
-                >
-                  No messages
-                </Heading>
-              )}
-            </TabPanel>
-            <TabPanel>
-              <p>two!</p>
-            </TabPanel>
-          </TabPanels>
+          {currentList?.length ? (
+            currentList.map((conversationInfo) => (
+              <ChatBox
+                isActive={isActive}
+                setIsActive={setIsActive}
+                src={conversationInfo.avatar}
+                name={conversationInfo.title}
+                info={conversationInfo.subtitle}
+                unreadCnt={conversationInfo.unread}
+                lastActivityTime={!isNaN(new Date(conversationInfo.date)) ? format(conversationInfo.date) : ''}
+                handleClick={() => handleClick(conversationInfo.title)}
+                id={conversationInfo.title}
+              />
+            ))
+          ) : (
+            <Heading as="h5" fontSize={"22px"} color="#2C5282" align={"center"}>
+              No messages
+            </Heading>
+          )}
         </Box>
-      </Tabs>
     </Box>
   );
 };
