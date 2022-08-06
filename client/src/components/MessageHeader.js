7b9a8222e7e5cebb47d796b72328cb96b5942323
linhtran <tranchilinh.0312@gmail.com>
2022-08-06 12:14:29 +0700

    format codestyle and add fallback image to message Header

--- client/src/components/MessageHeader.js
+++ client/src/components/MessageHeader.js
@@ -1,54 +1,101 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Box, Button, Flex, Heading, Image, Menu, MenuButton, MenuItem, MenuList, propNames, Stack, Tag, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  propNames,
+  Stack,
+  Tag,
+  Text,
+} from "@chakra-ui/react";
 import React, { forwardRef } from "react";
 
-const MessageHeader = ({ src, userName, lastMessage, onCloseConversation, prioritizeConversation, organization, assignConversation }) => {
-    const closeConversation = async () => {
-        await onCloseConversation();
-        window.location.href = "/conversations/all";
-    };
-    const markPrioritized = async () => {
-        await prioritizeConversation();
-        window.location.href = "/conversations/all";
-    };
-    return (
-        <Flex align="center" mb={"5"} width="100%" as={"a"} href="#" mt="3">
-            <Box width="60px">
-                <Image boxSize="100%" objectFit="cover" src={src} alt={userName} borderRadius="6px"/>
-            </Box>
+const MessageHeader = ({
+  src,
+  userName,
+  lastMessage,
+  onCloseConversation,
+  prioritizeConversation,
+  organization,
+  assignConversation,
+}) => {
+  const closeConversation = async () => {
+    await onCloseConversation();
+  };
+  const markPrioritized = async () => {
+    await prioritizeConversation();
+  };
+  return (
+    <Flex
+      align="center"
+      mb={"5"}
+      width="100%"
+      mt="3"
+      as="a"
+    >
+      <Box width="60px">
+        <Image
+          boxSize="100%"
+          objectFit="cover"
+          src={src}
+          alt={userName}
+          borderRadius="6px"
+          fallbackSrc="../fallbackImg.png"
+        />
+      </Box>
 
-            <Box flex={5} marginRight="5" alignItems="center" ml="3" maxWidth="50%">
-                <Heading as="h4" size={"sm"} color="#2C5282">
-                    {userName.slice(1, 20) + "..."}
-                </Heading>
-                <Text noOfLines={1} fontSize="sm">
-                    {lastMessage.message}
-                </Text>
-            </Box>
+      <Box flex={5} marginRight="5" alignItems="center" ml="3" maxWidth="50%">
+        <Heading as="h4" size={"sm"} color="#2C5282">
+          {userName.slice(1, 20) + "..."}
+        </Heading>
+        <Text noOfLines={1} fontSize="sm">
+          {lastMessage.message}
+        </Text>
+      </Box>
 
-            <Box flex={1} ml="auto" display={"flex"} py="2" justifyContent="right" alignItems={"center"}>
-                <Stack direction={"row"} alignItems="center" mr={"2"}>
+      <Box
+        flex={1}
+        ml="auto"
+        display={"flex"}
+        py="2"
+        justifyContent="right"
+        alignItems={"center"}
+      >
+        {/*  <Stack direction={"row"} alignItems="center" mr={"2"}>
                     <Text>Time: </Text>
                     <Tag bg={"pink.100"}>11:05</Tag>
-                </Stack>
-                <Menu>
-                    <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                        Options
-                    </MenuButton>
-                    <MenuList>
-                        {/*<MenuItem isDisabled>Download</MenuItem>
+                </Stack> */}
+        <Menu>
+          <MenuButton
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+            colorScheme="blue"
+          >
+            Options
+          </MenuButton>
+          <MenuList>
+            {/*<MenuItem isDisabled>Download</MenuItem>
             <MenuItem isDisabled>Create a Copy</MenuItem>
             <MenuItem isDisabled>Mark as Draft</MenuItem>
             <MenuItem isDisabled>Delete</MenuItem>
             <MenuItem isDisabled>Attend a Workshop</MenuItem>*/}
-                        <MenuItem onClick={() => markPrioritized()}>Mark as prioritized</MenuItem>
-                        <MenuItem onClick={() => closeConversation()}>Close conversation</MenuItem>
-                    </MenuList>
-                </Menu>
-                
-            </Box>
-        </Flex>
-    );
+            <MenuItem onClick={() => markPrioritized()}>
+              Mark as prioritized
+            </MenuItem>
+            <MenuItem onClick={() => closeConversation()}>
+              Close conversation
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Box>
+    </Flex>
+  );
 };
 
 export default MessageHeader;
