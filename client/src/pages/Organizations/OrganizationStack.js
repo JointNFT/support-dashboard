eb193ac1c799c3ca1531f47a404959ec4365187c
linhtran <tranchilinh.0312@gmail.com>
2022-08-06 12:11:49 +0700

    improve UI and add Loading component when fetching organizations

--- client/src/pages/Organizations/OrganizationStack.js
+++ client/src/pages/Organizations/OrganizationStack.js
@@ -1,15 +1,16 @@
 import { Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 
-const OrganizationStack = ({ fieldName, fieldData, fieldPercent, button, icon }) => {
+const OrganizationStack = ({ fieldName, fieldData, fieldPercent, button, icon, to }) => {
   return (
     <Stack bg="blue.50" rounded={"lg"} padding="10px" margin={"10px 0"}>
       <Text fontSize={"16px"}>{fieldName}</Text>
       <Flex justifyContent="space-between" alignItems={"center"}>
         <Heading fontSize={"18px"}>{fieldData}</Heading>
         {button && (
-          <Button colorScheme="gray" color="darkblue.100">
+          <Button colorScheme="gray" color="darkblue.100" as={Link} to={to}>
             {button}
           </Button>
         )}
