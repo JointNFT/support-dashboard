2a11754215035adbda15f6ea08728925e80b20e8
linhtran <tranchilinh.0312@gmail.com>
2022-08-06 17:21:24 +0700

    update closeConversation and updagteUserTag API

--- routes/conversation-route.js
+++ routes/conversation-route.js
@@ -4,19 +4,23 @@ const db = require("../utils/db");
  
 
 router.post("/updateUserTag",  async function (req, res) {
-    var payload = req.body;
-    var accessToken = payload.accessToken;
-    var userAddress = payload.userAddress;
-    var tag = payload.tag;
+    const payload = req.body;
+    const accessToken = payload.accessToken;
+    const userAddress = payload.userAddress;
+    const organizationId = payload.organizationId;
+    const createdBy = payload.createdBy;
+    const tag = payload.tag;
     if (accessToken != "" && userAddress != "") await db.updateUserTag(userAddress, accessToken, tag);
     if (organizationId != "" && createdBy != "") await db.updatePrioritizedConversations(organizationId, createdBy);
     res.send({ status: "success" });
 });
 
 router.post("/closeConversation",async function (req, res) {
-    var payload = req.body;
-    var accessToken = payload.accessToken;
-    var userAddress = payload.userAddress;
+    const payload = req.body;
+    const accessToken = payload.accessToken;
+    const userAddress = payload.userAddress;
+    const organizationId = payload.organizationId;
+    const createdBy = payload.createdBy;
     if (accessToken != "" && userAddress != "") await db.closeConversation(userAddress, accessToken);
     if (organizationId != "" && createdBy != "") await db.updateClosedConversations(organizationId, createdBy);
     res.send({ status: "success" });
